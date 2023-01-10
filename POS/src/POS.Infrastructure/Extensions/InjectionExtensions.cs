805843040714fb5de7abe2bb24c2e16644ec4bd3
CoreDX1 <chrismaquis@gmail.com>
2023-01-10 18:08:04 -0300

    Agregando la conecntion a la base de datos

--- POS/src/POS.Infrastructure/Extensions/InjectionExtensions.cs
+++ POS/src/POS.Infrastructure/Extensions/InjectionExtensions.cs
@@ -7,12 +7,20 @@ namespace POS.Infrastructure.Extensions
 {
     public static class InjectionExtensions
     {
-        public static IServiceCollection AddInjectionInfraestructure(this IServiceCollection services, IConfiguration configuration){
+        public static IServiceCollection AddInjectionInfraestructure(
+            this IServiceCollection services,
+            IConfiguration configuration
+        )
+        {
             var assembly = typeof(PostContext).Assembly.FullName;
             services.AddDbContext<PostContext>(
-                options => options.UseSqlServer(
-                    configuration.GetConnectionString("POSConnection"), b => b.MigrationsAssembly(assembly)), ServiceLifetime.Transient
-                );
+                options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("POSConnection"),
+                        b => b.MigrationsAssembly(assembly)
+                    ),
+                ServiceLifetime.Transient
+            );
             return services;
         }
     }
