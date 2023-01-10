dad8d3a44bbeaba24bfa591843831a57c290d3b1
CoreDX1 <chrismaquis@gmail.com>
2023-01-10 19:02:05 -0300

    Devolver los registros de la base de datos

---
+++ POS/src/POS.Infrastructure/Commons/Base/BaseEntityReponse.cs
@@ -0,0 +1,6 @@
+namespace POS.Infrastructure.Commons.Base;
+    // Devolver los registros de la base de datos
+    public class BaseEntityReponse
+    {
+        public int? TotalRecords { get; set; }
+    }
