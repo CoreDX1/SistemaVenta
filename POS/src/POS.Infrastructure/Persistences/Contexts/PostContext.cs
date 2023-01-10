78d92750709e88324eee4964d0b6123e30212661
CoreDX1 <chrismaquis@gmail.com>
2023-01-10 18:01:19 -0300

    Va a Buscar todos IEntityTypeConfiguration en las caeptas

--- POS/src/POS.Infrastructure/Persistences/Contexts/PostContext.cs
+++ POS/src/POS.Infrastructure/Persistences/Contexts/PostContext.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using POS.Domain.Entities;
 
-namespace POS.Domain.Entities;
+namespace POS.Infrastructure.Persistences.Contexts;
 
 public partial class PostContext : DbContext
 {
@@ -50,15 +52,10 @@ public partial class PostContext : DbContext
 
     public virtual DbSet<UsersBranchOffice> UsersBranchOffices { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        =>
-        optionsBuilder.UseSqlServer(
-            "Server=localhost;Database=POST;TrustServerCertificate=True;User id=sa;Password=index#12345"
-        );
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.HasAnnotation("Relational:Collation", "Modern_Spanish_CI_AS");
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         OnModelCreatingPartial(modelBuilder);
     }
 
